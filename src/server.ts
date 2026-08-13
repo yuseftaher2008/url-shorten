81655fefc7a3c13696d9359ccedfd4d7a924c6db
Yusef Taher <yuseftaher2008@hotmail.com>
2026-08-13 11:45:10 +0300

    set request body size limit

--- src/server.ts
+++ src/server.ts
@@ -9,8 +9,10 @@ const __dirname = path.dirname(__filename);
 
 const port: number = Number(process.env.PORT) || 3000;
 const app = express();
-
-app.use(express.json());
+// server middelware
+app.use(express.json(
+    {limit:'20kb'}
+));
 
 // Serve static frontend files
 app.use(express.static(path.join(__dirname, "view")));
